b9b0549ccaed06a6d23874804e569aaf6b809058
Aniket Yadav <aniketyadav690@gmail.com>
2023-10-30 21:18:16 +0530

    recipient's name

---
+++ script.js
@@ -0,0 +1,11 @@
+function autoFillRecipientName() {
+    // Get the input field element
+    var nameInput = document.getElementById("autoFillName");
+
+    // Get the recipient's name from the input field
+    var recipientName = nameInput.value;
+
+    // Find the element with the ID "recipient-name" and update its content
+    var recipientNameElement = document.getElementById("recipient-name");
+    recipientNameElement.textContent = recipientName;
+}
