b835b4725fd16b51a4bfbe68ae21a7a93714b8a1
Aniket Yadav <aniketyadav690@gmail.com>
2023-10-30 21:20:59 +0530

    logic corrected

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function autoFillRecipientName() {
-    // Get the input field element
-    var nameInput = document.getElementById("autoFillName");
+  // Get the input field element
+  var nameInput = document.getElementById("autoFillName");
 
-    // Get the recipient's name from the input field
-    var recipientName = nameInput.value;
+  // Get the recipient's name from the input field
+  var recipientName = nameInput.value;
 
-    // Find the element with the ID "recipient-name" and update its content
-    var recipientNameElement = document.getElementById("recipient-name");
-    recipientNameElement.textContent = recipientName;
+  // Find the element with the ID "recipient-name" and update its content
+  var recipientNameElement = document.getElementById("recipient-name");
+  recipientNameElement.textContent = recipientName;
+
+  nameInput.value = "";
 }
